182f63cf8686aae18d1d5ff72cda95a56158ff19
Renato Fagalde <renato@likwi.com.br>
2026-04-12 13:36:25 -0300

    Add unit tests for line count functionality in wc program

--- firstProgram/wc/main_test.go
+++ firstProgram/wc/main_test.go
@@ -5,15 +5,27 @@ import (
 	"testing"
 )
 
-func TestCoutingWords(t *testing.T) {
+func TestCountWords(t *testing.T) {
 	bufferString := bytes.NewBufferString("word1 word2 word3\n")
 
 	exp := 3
 
-	result := count(bufferString)
+	result := count(bufferString, false)
 
 	if result != exp {
 		t.Errorf("words count failed, expected %d, got %d", exp, result)
 	}
 
+}
+
+func TestCountLines(t *testing.T) {
+	bufferString := bytes.NewBufferString("word1 word2 word3\nword1 word2 word3\nword1 word2 word3\n")
+
+	exp := 3
+
+	result := count(bufferString, true)
+
+	if result != exp {
+		t.Errorf("lines count failed, expected %d, got %d", exp, result)
+	}
 }
