b79da86cc7c86228f59791d1f45550263c44fc2d
Renato Fagalde <renato@likwi.com.br>
2026-04-12 22:20:14 -0300

    Add support for byte count option in wc program

--- firstProgram/wc/main.go
+++ firstProgram/wc/main.go
@@ -5,18 +5,27 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
+
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(stdin io.Reader, countLines bool) int {
+func count(stdin io.Reader, countLines bool, bytes bool) int {
+
+	if bytes {
+		data, _ := ioutil.ReadAll(stdin)
+		return len(data)
+	}
 
 	scanner := bufio.NewScanner(stdin)
+
 	if !countLines {
 
 		scanner.Split(bufio.ScanWords)
