3ae9fab6628717c4e46b29f7b86b21f1e6ad4604
Renato Fagalde <renato@likwi.com.br>
2026-04-19 15:08:20 -0300

    :white_check_mark: Add unit tests for Complete method of List type

--- chapter-1/02-interacting/todo/todo_test.go
+++ chapter-1/02-interacting/todo/todo_test.go
@@ -15,3 +15,22 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Expected %q, got %q instead", taskName, list[0].Task)
 	}
 }
+
+func TestComplete(t *testing.T) {
+	list := todo.List{}
+
+	var taskName string = "New Task"
+	list.Add(taskName)
+
+	if list[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead", taskName, list[0].Task)
+	}
+	if list[0].Done {
+		t.Errorf("New task shouldn't be completed")
+	}
+
+	_ = list.Complete(1)
+	if !list[0].Done {
+		t.Errorf("New task shouldn't be completed")
+	}
+}
