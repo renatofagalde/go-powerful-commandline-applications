6ca727e454ce88b1c9bb29b3209af11ac69d88f8
Renato Fagalde <renato@likwi.com.br>
2026-04-19 21:36:09 -0300

    :white_check_mark: Add unit test for Delete method of List type

--- chapter-1/02-interacting/todo/todo_test.go
+++ chapter-1/02-interacting/todo/todo_test.go
@@ -34,3 +34,31 @@ func TestComplete(t *testing.T) {
 		t.Errorf("New task shouldn't be completed")
 	}
 }
+
+func TestDelete(t *testing.T) {
+	var l todo.List = todo.List{}
+
+	//slice
+	//var tasks []string = []string{"New Task 1", "New Task 2", "New Task 3"}
+	// se delcarar com o tamanho é um array
+	var tasks [3]string = [3]string{"New Task 1", "New Task 2", "New Task 3"}
+
+	for _, task := range tasks {
+		l.Add(task)
+	}
+
+	if l[0].Task != tasks[0] {
+		t.Errorf("Expected %q, got %q instead", tasks[0], l[0].Task)
+	}
+
+	_ = l.Delete(2)
+
+	if len(l) != 2 {
+		t.Errorf("Expected 2 items, got %d instead", len(l))
+	}
+
+	if l[1].Task != tasks[2] {
+		t.Errorf("Expected %q, got %q instead", tasks[2], l[1].Task)
+	}
+
+}
