d52829e17be6e32250a392dc7392cd921dc24566
Renato Fagalde <renato@likwi.com.br>
2026-04-27 22:13:09 -0300

    :sparkles: Add main package with initial command-line tool structure for todo app

---
+++ chapter-1/02-interacting/todo/cmd/todo/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	todo "bootstrap"
+	"os"
+)
+
+const TODO_FILE_NAME string = ".todo.json"
+
+func main() {
+	var list todo.List = todo.List{}
+
+	if err := list.Get(TODO_FILE_NAME); err != nil {
+		//errorString := fmt.Sprintf("Error %v to open the %s file", err, TODO_FILE_NAME)
+		//fmt.Println(errorString)
+
+		//Whe developing a command-line tool,
+		//it's a good practice to use the standard error(STDERR)
+		//output insetead of the standard output(STDOUT)
+		os.Exit(1)
+	}
+}
