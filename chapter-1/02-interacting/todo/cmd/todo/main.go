47c126279866d72939d7123ff1d5d4e7bb37de41
Renato Fagalde <renato@likwi.com.br>
2026-05-04 22:42:46 -0300

    :art: Comment out error handling block for improved maintainability and simplify main.go structure

--- chapter-1/02-interacting/todo/cmd/todo/main.go
+++ chapter-1/02-interacting/todo/cmd/todo/main.go
@@ -12,16 +12,16 @@ const TODO_FILE_NAME string = ".todo.json"
 func main() {
 	var list *todo.List = &todo.List{}
 
-	if err := list.Get(TODO_FILE_NAME); err != nil {
-		//errorString := fmt.Sprintf("Error %v to open the %s file", err, TODO_FILE_NAME)
-		//fmt.Println(errorString)
-
-		//Whe developing a command-line tool,
-		//it's a good practice to use the standard error(STDERR)
-		//output insetead of the standard output(STDOUT)
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	//if err := list.Get(TODO_FILE_NAME); err != nil {
+	//	//errorString := fmt.Sprintf("Error %v to open the %s file", err, TODO_FILE_NAME)
+	//	//fmt.Println(errorString)
+	//
+	//	//Whe developing a command-line tool,
+	//	//it's a good practice to use the standard error(STDERR)
+	//	//output insetead of the standard output(STDOUT)
+	//	fmt.Fprintln(os.Stderr, err)
+	//	os.Exit(1)
+	//}
 
 	switch {
 
