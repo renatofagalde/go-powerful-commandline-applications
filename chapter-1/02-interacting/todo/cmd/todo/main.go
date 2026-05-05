0a3d973a4b036cabbe0a8c53ef729f81d45d021f
Renato Fagalde <renato@likwi.com.br>
2026-05-04 22:47:15 -0300

    :art: Remove redundant comments and blank lines, simplify error handling in main.go

--- chapter-1/02-interacting/todo/cmd/todo/main.go
+++ chapter-1/02-interacting/todo/cmd/todo/main.go
@@ -11,19 +11,8 @@ const TODO_FILE_NAME string = ".todo.json"
 
 func main() {
 	var list *todo.List = &todo.List{}
-
-	//if err := list.Get(TODO_FILE_NAME); err != nil {
-	//	//errorString := fmt.Sprintf("Error %v to open the %s file", err, TODO_FILE_NAME)
-	//	//fmt.Println(errorString)
-	//
-	//	//Whe developing a command-line tool,
-	//	//it's a good practice to use the standard error(STDERR)
-	//	//output insetead of the standard output(STDOUT)
-	//	fmt.Fprintln(os.Stderr, err)
-	//	os.Exit(1)
-	//}
 	if err := list.Get(TODO_FILE_NAME); err != nil && !os.IsNotExist(err) {
-		fmt.Fprintln(os.Stderr, err)
+		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
